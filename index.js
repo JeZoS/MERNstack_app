2e49d17222f93f74e31a7b76365d4d53a2f15458
JeZoS <prabhat262111@gmail.com>
2020-06-23 02:06:20 +0530

    Prepare to deploy

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express=require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 connectDB();
 app.use(express.json({extended:false}));
 
-app.get('/', (req,res)=> res.send('API running'));
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/post',require('./routes/api/post'));
 app.use('/api/profiles',require('./routes/api/profiles'));
 app.use('/api/auth',require('./routes/api/auth'));
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+
+	app.get('*',(req,res) => {
+		res.sendfile(path.resolve(__dirname,'client','build','index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
